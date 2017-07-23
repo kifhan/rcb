a4f8b90e0a9a763315544b0c514c805aae108127
Jaguar Sparq <kifhan@gmail.com>
2017-07-23 21:57:42 +0900

    add transaction model

--- routes/transactions.js
+++ routes/transactions.js
@@ -11,4 +11,16 @@ router.get('/', isLogedin, function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 송금 요청, 청구
+
+
+
+// 대출 신청
+
+// 대출 현황 확인
+
+// 대출 상환
+
+
+
 module.exports = router;
